10eef48b8b0b7ea6a87c0114723997021eed62c1
JSomner94 <JSomner@iCloud.com>
2020-04-09 15:31:28 +0100

    Init

---
+++ src/main/java/com/pulselive/jsomner/league_table/LeagueTable.java
@@ -0,0 +1,22 @@
+package com.pulselive.jsomner.league_table;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jake Somner
+ */
+public class LeagueTable {
+    public LeagueTable(final List<Match> matches) {
+    }
+    
+    /**
+     * Get the ordered list of league table entries for this league table.
+     *
+     * @return
+     */
+    public List<LeagueTableEntry> getTableEntries() {
+        
+    }
+}
