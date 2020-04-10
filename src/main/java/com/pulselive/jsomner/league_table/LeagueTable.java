1a2c9bf013bae22e5f700485d6fbcdf52f9d904b
JSomner94 <JSomner@iCloud.com>
2020-04-10 15:21:36 +0100

    Add private List<LeagueTableEntry> tableEntries and fix Return statement for Getter

--- src/main/java/com/pulselive/jsomner/league_table/LeagueTable.java
+++ src/main/java/com/pulselive/jsomner/league_table/LeagueTable.java
@@ -8,22 +8,24 @@ import java.util.List;
  * @author Jake Somner
  */
 public class LeagueTable {
+
     public static void main(String[] args) {
         Match m1 = new Match("Charlton Athletic", "Everton", 1, 2);
         Match m2 = new Match("Derby", "Blackburn Rovers", 2, 1);
         Match m3 = new Match("Leeds", "Southampton", 2, 0);
-        
+
         List<Match> matches = new ArrayList<>();
         matches.add(m1);
         matches.add(m2);
         matches.add(m3);
-        
+
         System.out.println(matches);
-        
+
         LeagueTable lt = new LeagueTable(matches);
-        
+
         System.out.println(lt);
     }
+    private List<LeagueTableEntry> tableEntries;
 
     public LeagueTable(final List<Match> matches) {
     }
@@ -34,6 +36,6 @@ public class LeagueTable {
      * @return
      */
     public List<LeagueTableEntry> getTableEntries() {
-
+        return this.tableEntries;
     }
 }
