a5740d10422f154aca007183c1690ec51d62df08
JSomner94 <JSomner@iCloud.com>
2020-04-10 15:20:23 +0100

    Add Main Class (for testing) and reorganise members

--- src/main/java/com/pulselive/jsomner/league_table/LeagueTable.java
+++ src/main/java/com/pulselive/jsomner/league_table/LeagueTable.java
@@ -1,22 +1,39 @@
 package com.pulselive.jsomner.league_table;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Jake Somner
  */
 public class LeagueTable {
+    public static void main(String[] args) {
+        Match m1 = new Match("Charlton Athletic", "Everton", 1, 2);
+        Match m2 = new Match("Derby", "Blackburn Rovers", 2, 1);
+        Match m3 = new Match("Leeds", "Southampton", 2, 0);
+        
+        List<Match> matches = new ArrayList<>();
+        matches.add(m1);
+        matches.add(m2);
+        matches.add(m3);
+        
+        System.out.println(matches);
+        
+        LeagueTable lt = new LeagueTable(matches);
+        
+        System.out.println(lt);
+    }
+
     public LeagueTable(final List<Match> matches) {
     }
-    
+
     /**
      * Get the ordered list of league table entries for this league table.
      *
      * @return
      */
     public List<LeagueTableEntry> getTableEntries() {
-        
+
     }
 }
